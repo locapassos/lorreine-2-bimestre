476214f9eeac619fec46e62b600857cac56e94b5
locapassos <165210190+locapassos@users.noreply.github.com>
2024-04-18 17:32:44 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+        yRaqueteOponente += 10;
+    }
+
+
+}
+function incluiPlacar() {
+    stroke(255);
+    textAlign(CENTER);
+    textSize(16);
+    fill(color(255, 140, 0));
+    rect(150, 10, 40, 20);
+    fill(255);
+    text(meusPontos, 170, 26);
+    fill(color(255, 140, 0));
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosOponente, 470, 26);
+}
+
+function marcaPonto(){
+  if (xBolinha > 590){
+   meusPontos += 1;
+  }
+  if (xBolinha < 10){
+    pontosOponente += 1;
+  }
+  
+  
+}
